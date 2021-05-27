9cc2e59e5595bc48d52d1e1cbb24e9ccd1caeb13
Bennyziio <bapjoo2@gmail.com>
2021-05-27 21:42:51 +0900

    stripe를 통해 결제 기능 추가, favicon을 업데이트하고 title을 수정함

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51Ivi1XJ81LOFtlVLgpD8KAesSw9xZpYr3SJcJvMEDsGRJZxyqNVNq2OnctAw3XLadncZJozOPOFy13clAjEdSpWT00hLS2CbMi";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
