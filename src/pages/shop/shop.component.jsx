bd3e666b8de726d2dc5e7582d50c32df24621d20
Bennyziio <bapjoo2@gmail.com>
2021-06-11 23:18:28 +0900

    redux-thunk를 이용해서 fetchStart, fetchSuccess, fetchFailure를 통해 promise 대신 redux-thunk로 활용하게끔 수정

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 import { connect } from "react-redux";
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
+// import {
+//   firestore,
+//   convertCollectionsSnapshotToMap,
+// } from "../../firebase/firebase.utils";
 
-import { updateCollections } from "../../redux/shop/shop.actions";
+// import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
@@ -26,32 +29,29 @@ const ShopPage = ({ match }) => (
 */
 // change to class function
 class ShopPage extends React.Component {
-  state = {
-    loading: true,
-  };
+  // state = {
+  //   loading: true,
+  // };
 
-  unsubscribeFromSnapshot = null;
+  // unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
-
+    // const { updateCollections } = this.props;
+    // const collectionRef = firestore.collection("collections");
     // nasty method below
     // fetch(
     //   "https://firestore.googleapis.com/v1/projects/crwn-db-4ee0e/databases/(default)/documents/collections"
     // )
     //   .then((response) => response.json())
     //   .then((collections) => console.log(collections));
-
     // Promise Pattern
-    collectionRef.get().then((snapshot) => {
-      // console.log(snapshot);
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      // console.log(collectionsMap);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
-
+    // collectionRef.get().then((snapshot) => {
+    //   // console.log(snapshot);
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   // console.log(collectionsMap);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
     // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
     //   async (snapshot) => {
     //     // console.log(snapshot);
@@ -61,11 +61,13 @@ class ShopPage extends React.Component {
     //     this.setState({ loading: false });
     //   }
     // );
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
   }
 
   render() {
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching } = this.props;
+    // const { loading } = this.state;
 
     return (
       <div className="shop-page">
@@ -73,13 +75,19 @@ class ShopPage extends React.Component {
           exact
           path={`${match.path}`}
           render={(props) => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+            <CollectionsOverviewWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
         <Route
           path={`${match.path}/:collectionId`}
           render={(props) => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
+            <CollectionPageWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
       </div>
@@ -87,9 +95,14 @@ class ShopPage extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching,
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = (dispatch) => ({
+  // updateCollections: (collectionsMap) =>
+  //   dispatch(updateCollections(collectionsMap)),
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
